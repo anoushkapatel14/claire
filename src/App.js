f8cf0b88a4481f27fdc425e1564a75145ec98928
FirstName LastName <youremail@provider.com>
2024-01-24 18:11:02 +0000

    assets added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import ContactPage from "./pages/ContactPage/ContactPage";
+import EventsPage from "./pages/EventsPage/EventsPage";
+import ListenPage from "./pages/ListenPage/ListenPage";
+import TeachingPage from "./pages/TeachingPage/TeachingPage";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="events" element={<EventsPage />} />
+          <Route path="listen" element={<ListenPage />} />
+          <Route path="teaching" element={<TeachingPage />} />
+          <Route path="contact" element={<ContactPage />} />
+
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
-
-export default App;
